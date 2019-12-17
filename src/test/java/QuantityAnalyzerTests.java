c3ad4e1a023e4f060d118900ffe306e51dbc68e9
Akshay <akshaypatwari24@gmail.com>
2019-12-17 13:40:45 +0530

    Add:test case to check equality of two different value

--- src/test/java/QuantityAnalyzerTests.java
+++ src/test/java/QuantityAnalyzerTests.java
@@ -4,10 +4,18 @@ import org.junit.Test;
 public class QuantityAnalyzerTests
 {
    @Test
-   public void forOneFeetAndAnotherFeet_ShouldReturnEqual()
+   public void forOneFeetAndAnotherOneFeet_ShouldReturnEqual()
    {
-      Feet feet1 = new Feet(0.0);
-      Feet feet2 = new Feet(0.0);
+      Feet feet1 = new Feet(1.0);
+      Feet feet2 = new Feet(1.0);
       Assert.assertEquals(feet2, feet1);
+   }
+
+   @Test
+   public void givenOneFeetAndAnotherFeet_ShouldReturnNotEqual()
+   {
+      Feet feet1 = new Feet(1.0);
+      Feet feet2 = new Feet(2.0);
+      Assert.assertNotEquals(feet1,feet2);
    }
 }
