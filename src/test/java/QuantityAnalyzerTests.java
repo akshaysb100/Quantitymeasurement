1f0287955938d53eb232d298b490c5d8f6462bfe
Akshay <akshaypatwari24@gmail.com>
2019-12-17 13:33:45 +0530

    Add:test case to check  equality of two value

--- src/test/java/QuantityAnalyzerTests.java
+++ src/test/java/QuantityAnalyzerTests.java
@@ -1,6 +1,13 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class QuantityAnalyzerTests
 {
-
+   @Test
+   public void forOneFeetAndAnotherFeet_ShouldReturnEqual()
+   {
+      Feet feet1 = new Feet(0.0);
+      Feet feet2 = new Feet(0.0);
+      Assert.assertEquals(feet2, feet1);
+   }
 }
